6d7dad6b31705d89302e3c6667a588f8f706a433
Michael Satterfield <michael.f.satterfield@gmail.com>
2022-01-18 12:34:14 -0600

    added pipe funtion to parse data from .csv file

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const { parse } = require('csv-parse');
 const fs = require('fs');
 
+//empty array to be filled with data from .csv file
 const results = [];
 
 fs.createReadStream('kepler_data.csv')
+    //pipe function parses to readable data to writable
+    .pipe(parse({
+        comment: '#',
+        columns: true,
+    }))
     .on('data', (data) => {
-        results.push(data.data)
+        results.push(data)
     })
     .on('error', (err) => {
         console.log(err)
