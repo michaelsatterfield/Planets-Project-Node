19359db392c47836cc11bb1b25bd58a3bbea39a0
Michael Satterfield <michael.f.satterfield@gmail.com>
2022-01-17 22:50:37 -0600

    adding createReadStream event emitter

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const { parse } = require('csv-parse');
-const fs = require('fs')
+const fs = require('fs');
 
+const results = [];
+
+fs.createReadStream('kepler_data.csv')
+    .on('data', (data) => {
+        results.push(data.data)
+    })
+    .on('error', (err) => {
+        console.log(err)
+    })
+    .on('end', ()=> {
+        console.log(results);
+        console.log("done")
+    });
