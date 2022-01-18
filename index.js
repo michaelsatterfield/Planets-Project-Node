4b0e2df1e4dac47e2fe8757c4f008f193b816d90
Michael Satterfield <michael.f.satterfield@gmail.com>
2022-01-18 12:51:00 -0600

    added conditional for filtering confirmed planets

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const { parse } = require('csv-parse');
 const fs = require('fs');
 
 //empty array to be filled with data from .csv file
-const results = [];
+const habitablePlanet = [];
+
+//function to filter habitable planets
+const isHabitablePlanet = (planet) =>{
+    return planet['koi_disposition'] === 'CONFIRMED';
+}
 
 fs.createReadStream('kepler_data.csv')
     //pipe function parses to readable data to writable
@@ -11,12 +16,15 @@ fs.createReadStream('kepler_data.csv')
         columns: true,
     }))
     .on('data', (data) => {
-        results.push(data)
+        if(isHabitablePlanet(data)){
+        //pushes parsed data to habitablePlanet array above
+        habitablePlanet.push(data)
+        }
     })
     .on('error', (err) => {
         console.log(err)
     })
     .on('end', ()=> {
-        console.log(results);
+        console.log(habitablePlanet);
         console.log("done")
     });
